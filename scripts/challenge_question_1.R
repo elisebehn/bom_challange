29fdaeb78f20ac6478bfbe129294b9d1982ff3e3
Behn <beh028@csiro.au>
2020-03-17 10:56:53 +1100

    challange 1 answer

--- scripts/challenge_question_1.R
+++ scripts/challenge_question_1.R
@@ -1,3 +1,15 @@
 library(tidyverse)
 bom_data <- read_csv("data/BOM_data.csv")
-bom_stations <- read_csv("BOM_stations.csv")
+bom_stations <- read_csv("data/BOM_stations.csv")
+
+view(bom_stations)
+view(bom_data)
+
+
+ques_1 <- bom_data %>% 
+  separate(Temp_min_max,into = c("mean_temp","max_temp")) %>% 
+  filter(mean_temp != "" , max_temp != "", Rainfall != "-") %>% 
+  group_by(Station_number) %>% 
+  summarise(num_row = n())
+  
+           
